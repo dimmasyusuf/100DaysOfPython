2b9c998bddb1db9d4b3e06b07774a0dbb4e0aa8d
Dimas Yusuf Qurohman <96605743+dimmasyusuf@users.noreply.github.com>
2022-07-30 12:17:55 +0700

    Add files via upload
    Day 1 - "100DaysOfPython"

---
+++ Day 1 - Band Name Generator/band-generator.py
@@ -0,0 +1,13 @@
+#1. Create a greeting for your program.
+print("Welcome to the band name generator.")
+
+#2. Ask the user for their favorit color.
+color = input("What is your favorite color?\n")
+
+#3. Ask the user for the name of their pet.
+pet = input("What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + color + " " + pet)
+
+#5. Make sure the input cursor shows on a new line.

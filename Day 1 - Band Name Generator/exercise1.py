d06fc771c341a8b58a9d1cbfa2130be1304c8035
Dimas Yusuf Qurohman <96605743+dimmasyusuf@users.noreply.github.com>
2022-07-30 12:20:07 +0700

    Day 1
    Day 1 - "100DaysOfPython"

---
+++ Day 1 - Band Name Generator/exercise1.py
@@ -0,0 +1,12 @@
+# QUESTION
+
+# After you have written your code, you should run your program and it should print the following:
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# ANSWER:
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
